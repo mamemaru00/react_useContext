b1ecf9460122e9f75bfa3049c39fd8f4455d4e40
mamemaru00 <k.shouhei0711@gmail.com>
2023-08-31 22:17:31 +0900

    useContext count

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { createContext, useState } from 'react';
 import './App.css';
+import ComponentA from './components/ComponentA';
+
+export const UserCount = createContext();
 
 function App() {
+  const [count, setCount] = useState(100);
+  const value = {
+    count,
+    setCount,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Learn useContext</h1>
+      <UserCount.Provider value={value}>
+        <ComponentA />
+      </UserCount.Provider>
     </div>
   );
 }
